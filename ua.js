75274bd12a71d48fd8052e983c69d913eb6e48b6
Xian Dan <denghahae@gmail.com>
2020-01-13 19:55:59 +0800

    更新名单

--- ua.js
+++ ua.js
@@ -1,9 +1,9 @@
 //如果不是全局就得引入fs成员
-const fs = require("fs");
+const fs = require("fs")
 
 let args = process.argv.splice(2)
 
-function isIgnore(str) {
+function isIgnore (str) {
     for (let i = 0; i < args.length; i++) {
         if (str.indexOf(args[i]) !== -1) {
             return true
@@ -12,45 +12,49 @@ function isIgnore(str) {
     return false
 }
 
-fs.readFile("./blacklist.txt", "utf-8", function (error, data) {
-    let option = args[0]
-    args.splice(0, 1);
+function readUserAgentFileList (...path) {
+    const array = []
+    path.forEach(async (it) => {
+        const data = await fs.readFileSync(it, "utf-8")
+        const split = data.split("\n")
+        for (let i = 0; i < split.length; i++) {
+            let it = split[i].trim()
+            if (it.startsWith("#") || it.length <= 0) {
 
-    let array = new Array();
-
-    let split = data.split("\n");
-    for (let i = 0; i < split.length; i++) {
-        let it = split[i].trim()
-        if (it.startsWith("#") || it.length <= 0) {
-
-        } else if (isIgnore(it)) {
-            console.log("忽略", it)
-        } else {
-            array.push(it)
+            } else if (isIgnore(it)) {
+                console.info("忽略", it)
+            } else {
+                array.push(it)
+            }
         }
-    }
+    })
+    return array
+}
 
-    let str = ""
-    for (let i = 0; i < array.length; i++) {
-        if (option === "cf") {
-            //CloudFlare
+async function start () {
+    let option = args[0]
+    args.splice(0, 1)
+
+    const array = await readUserAgentFileList('./china-search-spider.txt', './foreign-search-spider.txt', './blacklist.txt',)
+
+    let str = ''
+    if (option === "regx") {
+        //正则表达式
+        str = `/${array.join('|')}/i`
+    } else if (option === "apache") {
+        //apache
+        str = array.join('|')
+    } else if (option === "cf") {
+        //CloudFlare
+        for (let i = 0; i < array.length; i++) {
             str += '(http.user_agent contains "' + array[i] + '")'
             if (i !== array.length - 1) {
                 str += ' or '
             }
-        } else if (option === "regx" || option === "apache") {
-            //正则表达式 Apache
-            str += array[i]
-            if (i !== array.length - 1) {
-                str += '|'
-            }
-        } else {
-            //换行
-            str += array[i]
-            if (i !== array.length - 1) {
-                str += '\n'
-            }
         }
+    } else {
+        //换行
+        str = '\n'
     }
 
     if (option === "apache") {
@@ -60,11 +64,13 @@ fs.readFile("./blacklist.txt", "utf-8", function (error, data) {
             "RewriteRule .* - [R=404,L]"
         fs.writeFile('./.htaccess', apache, function (err) {
             if (err) {
-                throw err;
+                throw err
             }
             console.log(apache)
-        });
+        })
     } else {
         console.log(str)
     }
-});
+}
+
+start()
